46a03bcde05ef07ab65a34e7dead4c7cea66724d
Phil <philippe.miranthis@gmail.com>
2025-07-17 09:01:36 +0100

    remove: sba custom model

--- custom_models/sba/__init__.py
+++
@@ -1,9 +0,0 @@
-from .config_sba import SBAConfig
-from .modeling_sba import SBAForCausalLM, SBAModel
-from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
-
-__all__ = ['SBAConfig', 'SBAForCausalLM', 'SBAModel']
-
-AutoConfig.register('sba', SBAConfig)
-AutoModel.register(SBAConfig, SBAModel)
-AutoModelForCausalLM.register(SBAConfig, SBAForCausalLM)
