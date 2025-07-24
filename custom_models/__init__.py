13c0e0a7f2a646d8e1d3e0d40c30ea9e04ea2ec6
Phil <philippe.miranthis@gmail.com>
2025-07-24 17:32:35 +0100

    fix data types

---
+++ custom_models/__init__.py
@@ -0,0 +1,1 @@
+import custom_models.lact_model
--- custom_models/lact_model/__init__.py
+++ custom_models/lact_model/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from .configuration_lact_swiglu import LaCTSWIGLUConfig
 from .modeling_lact import LaCTModel, LaCTForCausalLM
 
@@ -7,4 +8,16 @@ __all__ = ['LaCTSWIGLUConfig', 'LaCTModel', 'LaCTForCausalLM']
 
 AutoConfig.register("lact_swiglu", LaCTSWIGLUConfig)
 AutoModel.register(LaCTSWIGLUConfig, LaCTModel)
+=======
+from .configuration_lact_swiglu import LaCTSWIGLUConfig
+from .modeling_lact import LaCTModel, LaCTForCausalLM
+
+
+from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
+
+__all__ = ['LaCTSWIGLUConfig', 'LaCTModel', 'LaCTForCausalLM']
+
+AutoConfig.register("lact_swiglu", LaCTSWIGLUConfig)
+AutoModel.register(LaCTSWIGLUConfig, LaCTModel)
+>>>>>>> 72ddd32 (add: TTT files)
 AutoModelForCausalLM.register(LaCTSWIGLUConfig, LaCTForCausalLM)
