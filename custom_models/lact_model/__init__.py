1590be5df65d52502799f5069119b0c679c4ec55
Phil <philippe.miranthis@gmail.com>
2025-07-24 18:24:36 +0100

    fix merge

--- custom_models/lact_model/__init__.py
+++ custom_models/lact_model/__init__.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-from .configuration_lact_swiglu import LaCTSWIGLUConfig
-from .modeling_lact import LaCTModel, LaCTForCausalLM
 
-from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
-
-__all__ = ['LaCTSWIGLUConfig', 'LaCTModel', 'LaCTForCausalLM']
-
-AutoConfig.register("lact_swiglu", LaCTSWIGLUConfig)
-AutoModel.register(LaCTSWIGLUConfig, LaCTModel)
-=======
 from .configuration_lact_swiglu import LaCTSWIGLUConfig
 from .modeling_lact import LaCTModel, LaCTForCausalLM
 
@@ -19,5 +9,4 @@ __all__ = ['LaCTSWIGLUConfig', 'LaCTModel', 'LaCTForCausalLM']
 
 AutoConfig.register("lact_swiglu", LaCTSWIGLUConfig)
 AutoModel.register(LaCTSWIGLUConfig, LaCTModel)
->>>>>>> 72ddd32 (add: TTT files)
 AutoModelForCausalLM.register(LaCTSWIGLUConfig, LaCTForCausalLM)
